a02454a89c496173f1a664b14e142a432acbcae1
pangkuanxin <351477478@qq.com>
2022-05-27 17:25:09 +0800

    sumit the code

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include <windows.h>
+#include <QApplication>
+#include <QDebug>
+#include <QScreen>
+#include <WinUser.h>
+#include <iostream>
+#include <qpixmap.h>
+
+double calculateScaleFactor()
+{
+    auto activeWindow = GetActiveWindow();
+    HMONITOR monitor = MonitorFromWindow(activeWindow, MONITOR_DEFAULTTONEAREST);
+
+    // Get the logical width and height of the monitor
+    MONITORINFOEX monitorInfoEx;
+    monitorInfoEx.cbSize = sizeof(monitorInfoEx);
+    GetMonitorInfo(monitor, &monitorInfoEx);
+    auto cxLogical = monitorInfoEx.rcMonitor.right - monitorInfoEx.rcMonitor.left;
+    auto cyLogical = monitorInfoEx.rcMonitor.bottom - monitorInfoEx.rcMonitor.top;
+
+    // Get the physical width and height of the monitor
+    DEVMODE devMode;
+    devMode.dmSize = sizeof(devMode);
+    devMode.dmDriverExtra = 0;
+    EnumDisplaySettings(monitorInfoEx.szDevice, ENUM_CURRENT_SETTINGS, &devMode);
+    auto cxPhysical = devMode.dmPelsWidth;
+    auto cyPhysical = devMode.dmPelsHeight;
+
+    // Calculate the scaling factor
+    double horizontalScale = ((double)cxPhysical / (double)cxLogical);
+    double verticalScale = ((double)cyPhysical / (double)cyLogical);
+
+    std::cout << "Horizonzal scaling: " << horizontalScale << "\n";
+    std::cout << "Vertical scaling: " << verticalScale;
+
+    return horizontalScale > verticalScale ? verticalScale : horizontalScale;
+}
+
+
+int main(int argc, char *argv[])
+{
+    double scaleFactor = calculateScaleFactor();
+    // 让QIcon的pixmap可以自动适配，适合svg
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    qputenv("QT_SCALE_FACTOR", QByteArray::number(scaleFactor));
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ mainwindow.cpp
@@ -0,0 +1,57 @@
+﻿#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include<qdebug.h>
+#include <qpixmap.h>
+#include <qpainter.h>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // styleSheet结合text, text会在中间显示,与QIcon结合，图像会在左，字在右
+    // 图片很小张，重叠
+    ui->btn1->setStyleSheet("#btn1{border:none; width:100px; height:100px; border-image:url(:/image/send.svg)}");
+    // image是正常的，缩放正常, 不受AA_UseHighDpiPixmaps影响
+    ui->btn2->setStyleSheet("#btn2{border:none; width:400px; height:400px; background-image:url(:/image/send.svg)}");
+    // 单张，图像模糊
+    ui->btn3->setStyleSheet("#btn3{border:none; width:200px; height:200px; image:url(:/image/send.svg)}");  
+
+    //QIcon产生的svg图像是清晰的
+#if 0
+    qDebug() <<"C:/Users/kangaroo/Desktop/1.svg";
+    QIcon icon("C:/Users/kangaroo/Desktop/1.svg");
+    QPixmap p = icon.pixmap(200, 200);
+    ui->labelSvg->setPixmap(p);
+#endif
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent* event)
+{
+#if 0
+    QPainter painter(this);
+    // 加了SmoothPixmap感觉速度快了些
+    painter.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+    // 缩放后，x,y,width,height会自动扩大
+    //painter.drawRect(10, 10, 100, 100);
+
+    // 用icon画出的svg图片是可以缩放的
+#if 1
+    QIcon icon("C:/Users/kangaroo/Desktop/1.svg");
+    QPixmap p = icon.pixmap(900, 900);
+#else
+    // 直接画是会模糊
+    QPixmap p("C:/Users/kangaroo/Desktop/1.svg");
+#endif
+    painter.drawPixmap(0, 0, p);
+#else
+    QWidget::paintEvent(event);
+#endif
+}
+
